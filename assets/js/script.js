a6fc1e42b6f9d2191c168876853349f0c1d8e8cc
truethatplaya <alina.hernandez.ca@gmail.com>
2021-04-17 17:00:50 -0700

    landing page is working. start quiz button works and hides the landing page. js and css are properly connected to the html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 var quizContainer = document.getElementById("quiz");
 var resultsContainer = document.getElementById("results");
 var submitButton = document.getElementById("submit");
-var startButton;
-
-function showQuiz() {
-  var output = [];
-
-  myQuestions.forEach((element) => {});
-}
+var startButton = document.getElementById("startButton");
+var quizPage = document.getElementById("quizPage");
+var landingPage = document.getElementById("landingPage");
+var question = document.getElementById("question");
+var answerBtn1 = document.getElementById("answerBtn1");
+var answerBtn2 = document.getElementById("answerBtn2");
+var answerBtn3 = document.getElementById("answerBtn3");
+var answerBtn4 = document.getElementById("answerBtn4");
+var scorePage = document.getElementById("scorePage");
 
 //quiz quetions
 var quizQuestions = [
@@ -64,13 +66,41 @@ var quizQuestions = [
   },
 ];
 
+// timer - copied from activity
+function startTimer() {
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      gameOver();
+    }
+  }, 1000);
+}
+
+// show quiz function
+function showQuiz() {
+  var output = [];
+}
+
+// need logic for correct and wrong answers
+
+// show results of the quiz
 function showResults() {}
 
-//display quiz questions
-showQuiz();
-
 //on submit, show the results
-submitButton.addEventlistener("click", showResults);
+// submitButton.addEventlistener("click", showResults);
 
-//on start, show quiz questions
-startButton.addEventlistener("click", showQuiz);
+//on start, show quiz questions and start timer
+startButton.addEventListener("click", function () {
+  showQuiz();
+  startTimer();
+  landingPage.classList.add("hide");
+  quizPage.classList.remove("hide");
+});
+
+//on start up, a message is presented with how to take the quiz and button is avaiable to click to show quiz
+
+//NOTES
+// showQuiz(); - when this is in global scope this will start on load of page
+//TODO:make a note tab. look up in Google
